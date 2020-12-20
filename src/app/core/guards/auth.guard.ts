f72a507158eb4a9c8a53f617b30604bd6600eeb0
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-20 16:54:45 -0300

    feat: criando AuthGuard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { UserService } from '../services/user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.userService.user.value$.pipe(
+      map(user => !!user),
+      tap(logged => logged ? null : this.router.navigate(['/login']))
+    );
+  }
+}
