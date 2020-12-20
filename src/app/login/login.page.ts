caa714c093778d6c271172c6b13858c0576746fc
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-20 15:14:41 -0300

    feat: incluindo form na página de login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  initForm(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [ Validators.required, Validators.email ] ],
+      password: [ '', [ Validators.required, Validators.minLength(6) ]
+    ]});
+  }
+
+  submit(): void {
+  }
 }
