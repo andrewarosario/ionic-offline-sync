56cab1235f06e82ef8d427156c8104bf2103d21b
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-21 07:44:40 -0300

    feat: criando LoginGuard

---
+++ src/app/login/guards/login.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { UserService } from 'src/app/core/services/user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.userService.user.value$.pipe(
+      map(user => !user),
+      tap(notLogged => notLogged ? null : this.router.navigate(['/home']))
+    );
+  }
+}
