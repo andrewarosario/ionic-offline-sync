1f4b6d163496e65607277516f8c2c90dc4e0bff2
andrew.rosario <andrew.arosario@gmail.com>
2020-12-21 09:58:05 -0300

    feat: criando lista de itens

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
 import { ItemsPageRoutingModule } from './items-routing.module';
 
 import { ItemsPage } from './items.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     ItemsPageRoutingModule
   ],
   declarations: [ItemsPage]
