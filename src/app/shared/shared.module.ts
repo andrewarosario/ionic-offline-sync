fc1fea9a42cad21cd92d3daf0e3004e28731d348
Andrew Rosário <andrew.arosario@gmail.com>
2020-12-21 07:49:10 -0300

    refactor: movendo FormError para pasta de components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormErrorComponent } from './form-error/form-error.component';
+import { FormErrorComponent } from './components/form-error/form-error.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
